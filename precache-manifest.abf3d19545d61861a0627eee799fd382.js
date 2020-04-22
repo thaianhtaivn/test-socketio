993ea5e4bb90f9d3f805c862e3dc19c3807bf967
thaianhtaivn <thaianhtaivn>
2020-04-22 22:17:24 +0700

    Updates

---
+++ precache-manifest.abf3d19545d61861a0627eee799fd382.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae2eb593a8d9413682203c4285aeb3b",
+    "url": "/test-socketio/index.html"
+  },
+  {
+    "revision": "a9590fd294693460a73d",
+    "url": "/test-socketio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e10ca7d0946cc9670906",
+    "url": "/test-socketio/static/css/main.a49a96b6.chunk.css"
+  },
+  {
+    "revision": "a9590fd294693460a73d",
+    "url": "/test-socketio/static/js/2.da53a327.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/test-socketio/static/js/2.da53a327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10ca7d0946cc9670906",
+    "url": "/test-socketio/static/js/main.0bcba465.chunk.js"
+  },
+  {
+    "revision": "3a14e85128851be24416",
+    "url": "/test-socketio/static/js/runtime-main.fb581075.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-socketio/static/media/logo.5d5d9eef.svg"
+  }
+]);
